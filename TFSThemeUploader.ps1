deaaf8d31c5ab0f5aaf164495c07703417e7544a
Matteo Emili <matteo.emili@live.com>
2015-02-10 21:56:53 +0000

    Added a basic log infrastructure

--- TFSThemeUploader.ps1
+++ TFSThemeUploader.ps1
@@ -2,91 +2,147 @@
 # matteo.emili@live.com || http://mattvsts.blogspot.com || @MattVSTS
 # Basic parameters
 Param(
-[String]$TeamProject,
-[String]$CollectionURL
+[String]$CollectionURL,
+[String]$TeamProject
 )
 
 # Execution path and backup folder
 $data = Split-path $MyInvocation.MyCommand.Path
 $backupdata = New-Item -Force -ItemType Directory -Path "$data\Backup"
 
+
 # WITAdmin location (VS 2013)
 $WitAdmin = "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\witadmin.exe"
 
+$logline = "{0} - TFSThemeUploader started." -f (Get-Date).ToString("h:m:s")
+Add-Content -Path "$data\log.txt" -Value $logline -Force
+$logline = "{0} - Team Foundation Server: $CollectionURL" -f (Get-Date).ToString("h:m:s")
+Add-Content -Path "$data\log.txt" -Value $logline -Force
+$logline = "{0} - Team Project: $TeamProject" -f (Get-Date).ToString("h:m:s")
+Add-Content -Path "$data\log.txt" -Value $logline -Force
 
-# Get files to edit
-& $WitAdmin exportwitd /collection:$CollectionUrl /p:$TeamProject /n:Feature /f:"$backupdata\bck_Feature.xml"
-& $WitAdmin exportcategories /collection:$CollectionUrl /p:$TeamProject /f:"$backupdata\bck_Categories.xml"
-& $WitAdmin exportprocessconfig /collection:$CollectionUrl /p:$TeamProject /f:"$backupdata\bck_ProcessConfiguration.xml"
+try
+{
+    # Get files to edit
+    & $WitAdmin exportwitd /collection:$CollectionUrl /p:$TeamProject /n:Feature /f:"$backupdata\bck_Feature.xml"
+    & $WitAdmin exportcategories /collection:$CollectionUrl /p:$TeamProject /f:"$backupdata\bck_Categories.xml"
+    & $WitAdmin exportprocessconfig /collection:$CollectionUrl /p:$TeamProject /f:"$backupdata\bck_ProcessConfiguration.xml"
+    $logline = "{0} - Original Work Item Type, Categories List and Process Configuration exported." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+    Write-Output "Original files downloaded."
+}
+catch
+{
+    $logline = "{0} - Exception while backing up the original files." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+}
 
-Write-Output "Original files downloaded."
+
 
 # Modify the Feature Work Item Type to create a Theme Work Item Type
 # Change name, description and child filter
-$themeWIT = New-Object XML
-$themeWIT.Load("$backupdata\bck_Feature.xml")
-$themeWIT.WITD.WORKITEMTYPE.name = "Theme"
-$themeWIT.WITD.WORKITEMTYPE.DESCRIPTION = "Tracks a theme that is part of the overall strategy"
-
-foreach ($x in $themeWIT.WITD.WORKITEMTYPE.FORM.Layout.Group.Column.TabGroup.Tab.Where({$_.Label -eq "Implementation"}))
+try
 {
-    if ($x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType -eq "User Story")
+    $themeWIT = New-Object XML
+    $themeWIT.Load("$backupdata\bck_Feature.xml")
+    $themeWIT.WITD.WORKITEMTYPE.name = "Theme"
+    $themeWIT.WITD.WORKITEMTYPE.DESCRIPTION = "Tracks a theme that is part of the overall strategy"
+
+    foreach ($x in $themeWIT.WITD.WORKITEMTYPE.FORM.Layout.Group.Column.TabGroup.Tab.Where({$_.Label -eq "Implementation"}))
     {
-        $x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType = "Feature"
+        if ($x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType -eq "User Story")
+        {
+            $x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType = "Feature"
+        }
+        elseif ($x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType -eq "Product Backlog Item")
+        {
+            $x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType = "Feature"
+        }
     }
-    elseif ($x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType -eq "Product Backlog Item")
-    {
-        $x.Control.LinksControlOptions.WorkItemTypeFilters.Filter.WorkItemType = "Feature"
-    }
+    $themeWIT.Save("$data\Theme.xml")
+    $logline = "{0} - Theme Work Item Type created." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+    Write-Output "Theme created."
 }
-$themeWIT.Save("$data\Theme.xml")
+catch
+{
+    $logline = "{0} - Exception while creating the Theme Work Item Type." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+}
 
-Write-Output "Theme created."
 
-# Add a new Category for Themes
-$categories = New-Object XML
-$categories.Load("$backupdata\bck_Categories.xml")
 
-$themeCat = $categories.CATEGORIES.CATEGORY[0].Clone()
-$themeCat.refname = "Custom.ThemeCategory"
-$themeCat.name = "Theme Category"
-$themeCat.DEFAULTWORKITEMTYPE.name = "Theme"
+try
+{
+    # Add a new Category for Themes
+    $categories = New-Object XML
+    $categories.Load("$backupdata\bck_Categories.xml")
 
-$categories.DocumentElement.AppendChild($themeCat)
+    $themeCat = $categories.CATEGORIES.CATEGORY[0].Clone()
+    $themeCat.refname = "Custom.ThemeCategory"
+    $themeCat.name = "Theme Category"
+    $themeCat.DEFAULTWORKITEMTYPE.name = "Theme"
+    
+    $categories.DocumentElement.AppendChild($themeCat)
+    
+    $categories.Save("$data\Categories.xml")
+    $logline = "{0} - Custom.ThemeCategory Category created." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+    Write-Output "Category created."
+}
+catch
+{
+    $logline = "{0} - Exception while creating the Custom.ThemeCategory Category." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+}
 
-$categories.Save("$data\Categories.xml")
+try
+{
+    # Add a new Portfolio Backlog
+    # Set the Feature's PB parent attribute as it is no longer the topmost one
+    # Set the Theme's Portfolio Backlog attributes
+    # Create a new Work Item Color visualisation for Themes
+    $processconfig = New-Object XML  
+    $processconfig.Load("$backupdata\bck_ProcessConfiguration.xml")  
 
-Write-Output "Category created."
+    $themePB = $processconfig.ProjectProcessConfiguration.PortfolioBacklogs.PortfolioBacklog | Where-Object {$_.category -eq 'Microsoft.FeatureCategory'} 
+    $featurePB = $themePB.Clone() 
+    $featurePB.SetAttribute("parent", "Custom.ThemeCategory") 
+    $processconfig.ProjectProcessConfiguration.PortfolioBacklogs.AppendChild($featurePB) 
+    $themePB.category = "Custom.ThemeCategory"  
+    $themePB.pluralName = "Themes"  
+    $themePB.singularName = "Theme"
 
-# Add a new Portfolio Backlog
-# Set the Feature's PB parent attribute as it is no longer the topmost one
-# Set the Theme's Portfolio Backlog attributes
-# Create a new Work Item Color visualisation for Themes
-$processconfig = New-Object XML  
-$processconfig.Load("$backupdata\bck_ProcessConfiguration.xml")  
+    $basecolour = $processconfig.ProjectProcessConfiguration.WorkItemColors.WorkItemColor | Where-Object {$_.name -eq 'Feature'} 
+    $themecolour = $basecolour.Clone() 
+    $themecolour.SetAttribute("primary", "33CC33FF") 
+    $themecolour.SetAttribute("secondary", "009900FF")
+    $themecolour.SetAttribute("name", "Theme")
+    $processconfig.ProjectProcessConfiguration.WorkItemColors.AppendChild($themecolour)
 
-$themePB = $processconfig.ProjectProcessConfiguration.PortfolioBacklogs.PortfolioBacklog | Where-Object {$_.category -eq 'Microsoft.FeatureCategory'} 
-$featurePB = $themePB.Clone() 
-$featurePB.SetAttribute("parent", "Custom.ThemeCategory") 
-$processconfig.ProjectProcessConfiguration.PortfolioBacklogs.AppendChild($featurePB) 
-$themePB.category = "Custom.ThemeCategory"  
-$themePB.pluralName = "Themes"  
-$themePB.singularName = "Theme"
+    $processconfig.Save("$data\ProcessConfiguration.xml") 
+    $logline = "{0} - Theme Portfolio Backlog created." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+    Write-Output "Process Configuration updated."
+}
+catch
+{
+    $logline = "{0} - Exception while creating the Theme Portfolio Backlog." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+}
 
-$basecolour = $processconfig.ProjectProcessConfiguration.WorkItemColors.WorkItemColor | Where-Object {$_.name -eq 'Feature'} 
-$themecolour = $basecolour.Clone() 
-$themecolour.SetAttribute("primary", "33CC33FF") 
-$themecolour.SetAttribute("secondary", "009900FF")
-$themecolour.SetAttribute("name", "Theme")
-$processconfig.ProjectProcessConfiguration.WorkItemColors.AppendChild($themecolour)
-
-$processconfig.Save("$data\ProcessConfiguration.xml") 
-
-Write-Output "Process Configuration updated."
-
-# Import the new files into Team Foundation Server
-& $WitAdmin importwitd /collection:$CollectionUrl /p:$TeamProject /f:"$data\Theme.xml"
-& $WitAdmin importcategories /collection:$CollectionUrl /p:$TeamProject /f:"$data\Categories.xml"
-& $WitAdmin importprocessconfig /collection:$CollectionUrl /p:$TeamProject /f:"$data\ProcessConfiguration.xml"
-
-Write-Output "New files uploaded."
+try
+{
+    # Import the new files into Team Foundation Server
+    & $WitAdmin importwitd /collection:$CollectionUrl /p:$TeamProject /f:"$data\Theme.xml"
+    & $WitAdmin importcategories /collection:$CollectionUrl /p:$TeamProject /f:"$data\Categories.xml"
+    & $WitAdmin importprocessconfig /collection:$CollectionUrl /p:$TeamProject /f:"$data\ProcessConfiguration.xml"
+    $logline = "{0} - New Work Item Type, Categories List and Process Configuration imported." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+    Write-Output "New files uploaded."
+}
+catch
+{
+    $logline = "{0} - Exception while uploading the new files." -f (Get-Date).ToString("h:m:s")
+    Add-Content -Path "$data\log.txt" -Value $logline -Force
+}
